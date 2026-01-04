fed54cd2f6a8dcd46f63893a21f23533a0722588
safaamoussaoui00 <safaamousawi90@gmail.com>
2026-01-04 13:48:00 +0100

    Update recharger.js

--- recharger.js
+++ recharger.js
@@ -1,37 +1,27 @@
 import { recharger } from "../service/servicerecharger.js";
-
 const btnRecharger = document.getElementById("rechargerBtn");
 const amountInput = document.getElementById("amount");
 const cardSelect = document.getElementById("cardType");
-const balanceElement = document.getElementById("balance"); // si tu veux mettre à jour le dashboard
-
+const balanceElement = document.getElementById("balance");
 let user = JSON.parse(sessionStorage.getItem("currentUser"));
-
 btnRecharger.addEventListener("click", handleRecharge);
-
 async function handleRecharge() {
   const montant = Number(amountInput.value);
   const typeCarte = cardSelect.value;
-
   if (!montant || montant <= 0) {
     alert("Veuillez entrer un montant valide");
     return;
   }
-
   try {
     const description = `Recharge via ${typeCarte}`;
-
-    
     const updatedUser = await recharger(montant, user, description);
     sessionStorage.setItem("currentUser", JSON.stringify(updatedUser));
     if (balanceElement) balanceElement.textContent = updatedUser.balance + " DH";
-
-    alert(`Rechargement de ${montant} DH via ${typeCarte} réussi !`);
-    
+    alert(`Rechargement de ${montant} DH via ${typeCarte} reussi !`);
     amountInput.value = "";
     cardSelect.value = "Paypal";
-
   } catch (err) {
     alert(err);
   }
+
 }
